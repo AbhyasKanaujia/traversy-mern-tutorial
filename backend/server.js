d6470b63dd3a1e44bbfc92c5374133f6e6b3c1ff
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-02 20:12:44 +0530

    setup basic epxress server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log(`Hello World`);
+const express = require("express");
+require("dotenv").config();
+
+const PORT = 5000;
+
+const app = express();
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
