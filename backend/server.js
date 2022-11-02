bf59a16b1a62cf4d03d6758d4a2cb37ad8b3f620
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-02 22:08:08 +0530

    create route file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const goalRoutes = require("./routes/goalRoutes.js");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+app.use("/api/goals", goalRoutes);
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
