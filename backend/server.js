a86f7aab99aef3e5ff57632bbfb0b04f3cd3f7d0
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-03 12:04:51 +0530

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorHandler");
 const goalRoutes = require("./routes/goalRoutes.js");
 require("dotenv").config();
+const connectDB = require("./config/db.js");
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
