a41b949301d294e5550dd7d5795d9ee8a885a0e8
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-03 11:34:07 +0530

    Accepting body data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", goalRoutes);
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
