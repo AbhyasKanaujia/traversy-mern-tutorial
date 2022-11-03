301d53aa0b75924eb0681935f5fcf7a8004cb35b
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-03 14:26:42 +0530

    POST operation

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -20,7 +20,11 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field");
   }
 
-  res.status(200).json({ message: "Set Goal" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
 });
 
 // @desc    Set Goal
