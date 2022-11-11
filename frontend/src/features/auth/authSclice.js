e7269d1bfd5d774926440b3bdc04d57e03d49937
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 12:38:48 +0530

    add reducer in authSlice for login

--- frontend/src/features/auth/authSclice.js
+++ frontend/src/features/auth/authSclice.js
@@ -13,7 +13,6 @@ const initialState = {
 };
 
 // Register user
-
 export const register = createAsyncThunk(
   "auth/register",
   async (user, thunkAPI) => {
@@ -32,6 +31,21 @@ export const register = createAsyncThunk(
     }
   }
 );
+
+// Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    console.log(error);
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
