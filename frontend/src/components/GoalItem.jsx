9032084278dc7fbc42dce4dcb355e707e0115717
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 17:21:28 +0530

    add close button and dispatch delete goal

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,8 +1,16 @@
+import { useDispatch } from "react-redux";
+import deleteGoal from "../features/goals/goalSlice";
+
 function GoalItem({ goal }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="goal">
       <div>{new Date(goal.createdAt).toLocaleString("en-IN")} </div>
       <h3>{goal.text}</h3>
+      <button className="close" onClick={() => dispatch(deleteGoal(goal._id))}>
+        X
+      </button>
     </div>
   );
 }
