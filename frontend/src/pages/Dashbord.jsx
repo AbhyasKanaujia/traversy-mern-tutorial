2068d5513d03c197f2ba7c55f03eaa0ad3fdf8b1
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 15:29:03 +0530

    restrict dashboard to logged in user

--- frontend/src/pages/Dashbord.jsx
+++ frontend/src/pages/Dashbord.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Dashbord() {
+  const navigate = useNavigate();
+
   const { user } = useSelector((store) => store.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return <div>Dashbord</div>;
 }
 
