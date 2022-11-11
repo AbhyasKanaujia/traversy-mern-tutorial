793c422543f712b3f98a32fbdf1bed7e9ebac198
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 15:52:41 +0530

    use dummy createGoal reducer

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-
-const onSubmit = (e) => {
-  e.preventDefault();
-};
+import { useDispatch } from "react-redux";
+import { createGoal } from "../features/goals/goalSlice";
 
 function GoalForm() {
   const [text, setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createGoal({ text }));
+    setText("");
+  };
 
   return (
     <section className="form">
