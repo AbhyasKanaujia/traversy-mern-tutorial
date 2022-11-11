b929c6aa361c8f6b615bf1d9d3e0853e844b2aec
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 14:56:33 +0530

    import statements copied from register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaSignInAlt } from "react-icons/fa";
+import { login, reset } from "../features/auth/authSclice";
+import Spinner from "../components/Spinner";
 
 function Login() {
   const [formData, setFormData] = useState({
