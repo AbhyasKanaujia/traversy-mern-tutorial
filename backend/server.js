e5bf425a3773145b6b1fec371ba971b01f05b620
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2022-11-11 18:14:18 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorHandler");
 const goalRoutes = require("./routes/goalRoutes.js");
@@ -17,6 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Please set to production");
+  });
+}
 
 app.use(errorHandler);
 
